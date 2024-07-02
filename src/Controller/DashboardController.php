3a485d735191b480d7afcee59615712e866e05c6
Wickeur <wicke.julian@gamil.com>
2024-07-02 14:47:07 +0200

    feat(connexion): style login et register

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/', name: 'dashboard')]
+    #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
